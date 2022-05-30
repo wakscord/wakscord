cded593a9e97a88312213a3dcf58117f6c40b08b
minibox24 <minibox724@gmail.com>
2022-05-30 22:55:13 +0900

    구독

--- src/components/DiscordModal.tsx
+++ src/components/DiscordModal.tsx
@@ -9,15 +9,22 @@ import {
   ModalBody,
   ModalCloseButton,
   Button,
-} from "@chakra-ui/react";
-
-import {
   Input,
   FormControl,
   FormLabel,
   FormErrorMessage,
-  FormHelperText,
+  Text,
+  Divider,
+  useToast,
+  Collapse,
+  useDisclosure,
+  Flex,
 } from "@chakra-ui/react";
+
+import { Icon, ChevronUpIcon, ChevronDownIcon } from "@chakra-ui/icons";
+
+import Option, { Data } from "./Option";
+import { API_BASE_URL, ITEMS } from "../constants";
 
 const webhookRegex =
   /discord(?:app)?.com\/api\/webhooks\/([0-9]{17,20})\/([A-Za-z0-9.\-_]{60,68})/;
@@ -33,6 +40,138 @@ export default function DiscordModal({ isOpen, onClose }: IDModalProp) {
   const handleInputChange = (e: React.FormEvent<HTMLInputElement>) =>
     setInput((e.target as HTMLInputElement).value);
   const isError = !webhookRegex.test(input);
+  const { isOpen: optionIsOpen, onToggle: optionOnToggle } = useDisclosure();
+  const toast = useToast();
+
+  const [data, setData] = useState<{
+    [name: string]: Data;
+  }>({
+    아이네: {
+      enabled: true,
+      bangon: true,
+      title: true,
+      users: Object.keys(ITEMS),
+    },
+    징버거: {
+      enabled: true,
+      bangon: true,
+      title: true,
+      users: Object.keys(ITEMS),
+    },
+    릴파: {
+      enabled: true,
+      bangon: true,
+      title: true,
+      users: Object.keys(ITEMS),
+    },
+    주르르: {
+      enabled: true,
+      bangon: true,
+      title: true,
+      users: Object.keys(ITEMS),
+    },
+    고세구: {
+      enabled: true,
+      bangon: true,
+      title: true,
+      users: Object.keys(ITEMS),
+    },
+    비챤: {
+      enabled: true,
+      bangon: true,
+      title: true,
+      users: Object.keys(ITEMS),
+    },
+    우왁굳: {
+      enabled: true,
+      bangon: true,
+      title: true,
+      users: Object.keys(ITEMS),
+    },
+    뢴트게늄: { enabled: false, bangon: false, title: false, users: [] },
+  });
+
+  const handleChange = (name: string, _data: Data) => {
+    setData({ ...data, [name]: _data });
+  };
+
+  const sub = async () => {
+    const _data: {
+      url: string;
+      subs: {
+        [name: string]: string[];
+      };
+    } = { url: input, subs: {} };
+
+    Object.entries(data).forEach(([name, data]) => {
+      if (data.enabled) {
+        _data.subs[name] = data.users;
+
+        if (data.bangon) {
+          _data.subs[name].push("뱅온");
+        }
+
+        if (data.title) {
+          _data.subs[name].push("방제");
+        }
+      }
+    });
+
+    const res = await fetch(API_BASE_URL + "/subscribe", {
+      method: "POST",
+      body: JSON.stringify(_data),
+    });
+
+    if (res.status !== 204) {
+      const json = await res.json();
+
+      onClose();
+      toast({
+        title: "오류 발생",
+        description: json.message,
+        status: "error",
+        duration: 10000,
+        isClosable: true,
+      });
+    } else {
+      onClose();
+      toast({
+        title: "구독 완료",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const unsub = async () => {
+    const res = await fetch(API_BASE_URL + "/unsubscribe", {
+      method: "POST",
+      body: JSON.stringify({ url: input }),
+    });
+
+    if (res.status !== 204) {
+      const json = await res.json();
+
+      onClose();
+
+      toast({
+        title: "오류 발생",
+        description: json.message,
+        status: "error",
+        duration: 10000,
+        isClosable: true,
+      });
+    } else {
+      onClose();
+      toast({
+        title: "구독 취소 완료",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -43,7 +182,9 @@ export default function DiscordModal({ isOpen, onClose }: IDModalProp) {
 
         <ModalBody>
           <FormControl isInvalid={isError}>
-            <FormLabel htmlFor="email">디스코드 웹후크 URL</FormLabel>
+            <FormLabel htmlFor="email" fontWeight="bold">
+              디스코드 웹후크 URL
+            </FormLabel>
             <Input
               id="email"
               type="email"
@@ -56,15 +197,42 @@ export default function DiscordModal({ isOpen, onClose }: IDModalProp) {
               </FormErrorMessage>
             )}
           </FormControl>
+
+          <Divider mt={5} mb={5} />
+
+          <Flex alignItems="center" mb={3}>
+            <Text fontWeight="bold" fontSize="1.2rem">
+              설정
+            </Text>
+            <Icon
+              as={optionIsOpen ? ChevronUpIcon : ChevronDownIcon}
+              w={10}
+              h={10}
+              onClick={optionOnToggle}
+            />
+          </Flex>
+
+          <Collapse in={optionIsOpen} style={{ overflow: "initial" }}>
+            {Object.keys(ITEMS).map((x, idx) => (
+              <Option
+                key={idx}
+                name={x}
+                value={data[x]}
+                onChange={handleChange}
+              />
+            ))}
+          </Collapse>
         </ModalBody>
 
         <ModalFooter>
           {!isError && (
             <>
-              <Button colorScheme="red" mr={3}>
+              <Button colorScheme="red" mr={3} onClick={unsub}>
                 구독 취소
               </Button>
-              <Button colorScheme="green">구독</Button>
+              <Button colorScheme="green" onClick={sub}>
+                구독
+              </Button>
             </>
           )}
         </ModalFooter>
---
+++ src/components/Option.tsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react";
+
+import {
+  Switch,
+  Text,
+  Flex,
+  Box,
+  Checkbox,
+  Collapse,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+import { MultiSelect } from "react-multi-select-component";
+
+import { AVAILABLE_ITEMS } from "../constants";
+
+export interface Data {
+  enabled: boolean;
+  bangon: boolean;
+  title: boolean;
+  users: string[];
+}
+
+interface IOptionProp {
+  name: string;
+  value: Data;
+  onChange: (name: string, data: Data) => void;
+}
+
+export default function Option({ name, value, onChange }: IOptionProp) {
+  const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: value.enabled });
+  const [bangon, setBangon] = useState<boolean>(value.bangon);
+  const [title, setTitle] = useState<boolean>(value.title);
+  const [selected, setSelected] = useState(
+    value.users.map((x) => {
+      return { label: x, value: x };
+    })
+  );
+
+  useEffect(() => {
+    onChange(name, {
+      enabled: isOpen,
+      bangon,
+      title,
+      users: selected.map((x) => x.value),
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen, bangon, title, selected]);
+
+  return (
+    <Box mb={3}>
+      <Flex alignItems="center">
+        <Switch size="lg" isChecked={isOpen} onChange={onToggle} />
+        <Text ml={2} fontSize="1.3rem" fontWeight="bold">
+          {name}
+        </Text>
+      </Flex>
+
+      <Collapse in={isOpen} style={{ overflow: "initial" }}>
+        <Box mt={1}>
+          <Flex justifyContent="center" gap={3}>
+            <Checkbox
+              defaultChecked
+              onChange={(e) => {
+                setBangon(e.target.checked);
+              }}
+            >
+              뱅온 알림
+            </Checkbox>
+            <Checkbox
+              defaultChecked
+              onChange={(e) => {
+                setTitle(e.target.checked);
+              }}
+            >
+              방제 변경 알림
+            </Checkbox>
+          </Flex>
+
+          <MultiSelect
+            options={AVAILABLE_ITEMS.map((x) => {
+              return { label: x, value: x };
+            })}
+            value={selected}
+            onChange={setSelected}
+            labelledBy="Select"
+            overrideStrings={{
+              allItemsAreSelected: "모두 선택됨",
+              noOptions: "결과 없음",
+              search: "검색",
+              selectAll: "전체 선택하기",
+              selectAllFiltered: "전체 선택 (검색)",
+              selectSomeItems: "선택하기",
+            }}
+          />
+        </Box>
+      </Collapse>
+    </Box>
+  );
+}
