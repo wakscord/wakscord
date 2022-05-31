76d21331e1d2b22d1675b7c53e93c6bb9c2486f7
minibox24 <minibox724@gmail.com>
2022-05-31 21:41:07 +0900

    ga4

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
+
+ReactGA.initialize("G-9REMLTNZDT");
+ReactGA.send("pageview");
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,5 +19,3 @@ root.render(
     </ChakraProvider>
   </React.StrictMode>
 );
-
-ReactGA.initialize("G-9REMLTNZDT");
