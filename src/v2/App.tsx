26310bf79321fe156fe1079c4b2f3b597665ca2d
minibox24 <minibox724@gmail.com>
2022-09-25 20:44:08 +0900

    test

--- src/v2/App.tsx
+++ src/v2/App.tsx
@@ -1,5 +1,36 @@
 import { useEffect, useState } from "react";
 
+import Message from "./components/discord/Message";
+
+import { IMessage } from "./types";
+
 export default function App() {
-  return <div className="App"></div>;
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://api.isedol-cctv.xyz/messages/viichan"
+      );
+      const messages = await response.json();
+      setMessages(messages);
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      <div
+        style={{
+          background: "#36393f",
+          width: "500px",
+          height: "500px",
+          overflowY: "scroll",
+        }}
+      >
+        {messages.map((message, idx) => (
+          <Message message={message} before={messages[idx - 1]} key={idx} />
+        ))}
+      </div>
+    </div>
+  );
 }
