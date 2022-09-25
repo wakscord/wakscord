dee445ee90f4f5d2ffa491f96c97f4ebb9bc527c
minibox24 <minibox724@gmail.com>
2022-09-25 20:44:13 +0900

    types

---
+++ src/v2/types.ts
@@ -0,0 +1,24 @@
+export interface IDiscordEmbed {
+  url: string;
+  title: string;
+  color: number;
+  timestamp?: string;
+  description?: string;
+  image?: { url: string };
+  author?: { name: string };
+  fields?: { name: string; value: string; inline: boolean }[];
+}
+
+export interface IDiscordMessage {
+  username: string;
+  avatar_url: string;
+  content: string;
+  embeds?: IDiscordEmbed[];
+}
+
+export interface IMessage {
+  id: number;
+  data: IDiscordMessage;
+  count: number;
+  sended_at: string;
+}
